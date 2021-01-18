421f2d5a6c451dc24c7d94c24f2b9660a8dc3e3d
jjwon915 <jjwon915@naver.com>
2021-01-18 22:31:52 +0900

    크롤링할 키워드와 페이지 수를 입력받는다. 해당 키워드가 있는 뉴스 제목을 페이지 수 만큼 for문을 실행해 크롤링한 후 daum_list에 append한다. daum_list를 result.html로 전달해 list형태로 출력한다.

--- app.py
+++ app.py
@@ -15,13 +15,17 @@ def result():
     keyword = request.form['input1']
     page = request.form['input2']
 
-    req = requests.get("https://search.daum.net/search?w=news&nil_search=btn&DA=NTB&enc=utf8&cluster=y&cluster_page=1&q=" + keyword)
-    soup = BeautifulSoup(req.text, 'html.parser')
+    daum_list = []
 
-    for i in soup.find_all("a", class_="f_link_b"):
-        print(i.text)
+    for i in range(1, int(page)+1):
+        req = requests.get("https://search.daum.net/search?w=news&DA=PGD&enc=utf8&cluster=y&cluster_page=1&q=" + keyword + "&p=" + page)
+        soup = BeautifulSoup(req.text, 'html.parser')
 
-    return render_template("result.html")
+        for i in soup.find_all("a", class_="f_link_b"):
+            print(i.text)
+            daum_list.append(i.text)
+
+    return render_template("result.html", daum_list = daum_list)
 
 if __name__ == '__main__':
     app.run()
