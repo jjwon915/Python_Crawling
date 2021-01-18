5b998fc7a35635f534b20dbde7bf0a851e336cb9
jjwon915 <jjwon915@naver.com>
2021-01-18 22:11:31 +0900

    다음 뉴스에서 키워드로 검색한 제목들을 크롤링 해오기 위한 코드. 다음 뉴스의 제목들은 a태그 안에 담겨있으며 class명으로는 f_link_b를 사용한다.

--- app.py
+++ app.py
@@ -2,14 +2,24 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+import requests
+from bs4 import BeautifulSoup
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
 
 @app.route('/result', methods=['POST'])
 def result():
-    print(request.form['input1'])
-    print(request.form['input2'])
+
+    keyword = request.form['input1']
+    page = request.form['input2']
+
+    req = requests.get("https://search.daum.net/search?w=news&nil_search=btn&DA=NTB&enc=utf8&cluster=y&cluster_page=1&q=" + keyword)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    for i in soup.find_all("a", class_="f_link_b"):
+        print(i.text)
 
     return render_template("result.html")
 
