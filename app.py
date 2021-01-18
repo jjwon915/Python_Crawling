eff4305e561a638a7d21b5000a8fc92470bcd7a3
jjwon915 <jjwon915@naver.com>
2021-01-18 22:49:44 +0900

    크롤링한 데이터를 엑셀에 저장해 다운로드 할 수 있도록 하기 위한 코드를 작성하였다. 엑셀에 저장되기 위해서는 1,1 위치부터 저장되어야 하므로 for i in range(1, len(daum_list) + 1):   write_ws.cell(i,1, daum_list[i-1]) 이렇게 작성해주었다. result.html에서 static폴더 밑에 저장된 result.xlsx을 다운로드 받을 수 있도록 했다.

--- app.py
+++ app.py
@@ -5,6 +5,11 @@ app = Flask(__name__)
 import requests
 from bs4 import BeautifulSoup
 
+# 엑셀 사용을 위한 준비
+from openpyxl import Workbook
+write_wb = Workbook()
+write_ws = write_wb.active
+
 @app.route('/')
 def hello_world():
     return render_template("index.html")
@@ -18,12 +23,17 @@ def result():
     daum_list = []
 
     for i in range(1, int(page)+1):
-        req = requests.get("https://search.daum.net/search?w=news&DA=PGD&enc=utf8&cluster=y&cluster_page=1&q=" + keyword + "&p=" + page)
+        req = requests.get("https://search.daum.net/search?w=news&DA=PGD&enc=utf8&cluster=y&cluster_page=1&q=" + keyword + "&p=" + str(i))
         soup = BeautifulSoup(req.text, 'html.parser')
 
         for i in soup.find_all("a", class_="f_link_b"):
             print(i.text)
             daum_list.append(i.text)
+
+        for i in range(1, len(daum_list) + 1):
+            write_ws.cell(i,1, daum_list[i-1])
+
+        write_wb.save("static/result.xlsx")
 
     return render_template("result.html", daum_list = daum_list)
 
